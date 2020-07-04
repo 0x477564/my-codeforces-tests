f262ff27bb3bf00b49850133a92935b7206c6b99
0x477564 <45697827+0x477564@users.noreply.github.com>
2020-07-04 15:30:30 +0200

    Add files via upload

---
+++ 630A Again Twenty Five!.rs
@@ -0,0 +1,15 @@
+/*
+    Number theory - https://codeforces.com/problemset/problem/630/A
+    Completed: 4/7/20 15:30
+*/
+
+use std::io;
+
+fn main() {
+    let mut input = String::new();
+    io::stdin().read_line(&mut input).unwrap();
+
+    // This is so retarded
+    println!("25");
+
+}
